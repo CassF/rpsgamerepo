957db55bc321ea187d458212318a051f9ac79b7c
CassF <cassandrafrimpong@gmail.com>
2016-11-29 16:37:55 +0000

    added compare function

--- js/app.js
+++ js/app.js
@@ -4,24 +4,41 @@ $(function() {
     // Array containing possible outcomes
     let result = ["wins","losses","ties"];
 
-        // Event listener for when button is clicked
-    button.addEventListener("click" , function() {
-            
-        // Generates a random whole number - 0, 1 or 2 and assigns it to computerChoice
-        let computerChoice = moves[Math.floor((Math.random() * 3) + 0)];
-        
-        console.log(computerChoice);
-        console.log('Button was clicked');
-                    
-        // When an element with the class "play" is clicked  the following function will run
-        $('.play').click(function() {
+    function compResult () {
+            return moves[Math.floor((Math.random() * 3) + 0)];     
+    }
+
+    $('.play').click(function() {
             // Sets the variable clickedID to the id assigned to the button 
             let clickedID = this.id;
             console.log(clickedID);
-
-
-        });
     });
 
+    function compareResults (clickedID, compResult) {
+            if (clickedID === "rock" && compResult=== "paper") {
+                printResult(true, false, false);
+            } else if (clickedID === "paper" && compResult === "rock"){
+                printResult (true, false, fals);
+            } else if (clickedID === "scissors" && compResult === "paper"){
+                printResult (true, false, false);
+            } else if (compResult === "rock" && clickedID === "scissors"){
+                printResult (false, true, false);
+            } else if (compResult === "paper" && clickedID === "rock"){
+                printResult (false, true, false);  
+            } else if (compResult === "scissors" && clickedID === "paper"){
+                printResult (false, true, false);
+            } else { 
+                printResult (false, false, true);
+            }
+    }
+
+    function printResult(playerWins, computerWins, draw){
+        if (playerWins){
+            let playerScore = parseInt($('#playerScore').html());
+            playerScore++;
+            $('#playerScore').html(playerScore);
+            $('#result').html("Player wins");
+        }
+    }
 });
 
