fe68689c266d13955a3e3461a59421064c68cbf5
CassF <cassandrafrimpong@gmail.com>
2016-11-29 17:07:14 +0000

    completed code, styling not finished

--- js/app.js
+++ js/app.js
@@ -4,21 +4,23 @@ $(function() {
     // Array containing possible outcomes
     let result = ["wins","losses","ties"];
 
-    function compResult () {
-            return moves[Math.floor((Math.random() * 3) + 0)];     
+    function compResult() {
+        return moves[Math.floor((Math.random() * 3) + 0)];
     }
 
     $('.play').click(function() {
             // Sets the variable clickedID to the id assigned to the button 
             let clickedID = this.id;
-            console.log(clickedID);
+            // console.log(clickedID);
+            compareResults(clickedID, compResult());
     });
 
     function compareResults (clickedID, compResult) {
+        // console.log(clickedID, compResult);
             if (clickedID === "rock" && compResult=== "paper") {
                 printResult(true, false, false);
             } else if (clickedID === "paper" && compResult === "rock"){
-                printResult (true, false, fals);
+                printResult (true, false, false);
             } else if (clickedID === "scissors" && compResult === "paper"){
                 printResult (true, false, false);
             } else if (compResult === "rock" && clickedID === "scissors"){
@@ -30,15 +32,27 @@ $(function() {
             } else { 
                 printResult (false, false, true);
             }
+
+            
+            
     }
 
     function printResult(playerWins, computerWins, draw){
+        console.log(computerWins,playerWins, draw);
         if (playerWins){
             let playerScore = parseInt($('#playerScore').html());
             playerScore++;
             $('#playerScore').html(playerScore);
             $('#result').html("Player wins");
-        }
+            } else 
+            if (computerWins) {
+            let computerScore = parseInt ($("#computerScore").html());
+            computerScore ++;
+            $ ("#computerScore").html(computerScore);
+            $ ("#result").html("Computer wins!");
+            } else {
+            $ ("#result").html("It's a draw!");
+            }
     }
 });
 
